d693fbf5441d26a7e9109267e011b77027abe503
Hydra008 <kshah2@lakeheadu.ca>
2017-08-15 15:34:02 -0400

    Update Solution.java

--- Strings/Java String Introdution/Solution.java
+++ Strings/Java String Introdution/Solution.java
@@ -8,25 +8,25 @@ public class Solution {
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String B=sc.next();
+        sc.close();
         /* Enter your code here. Print output to STDOUT. */
         int length = A.length()+B.length();
         System.out.println(length);
         char characterA = A.charAt(0);
-        int asciiA = (int) characterA;
+       
         char characterB = B.charAt(0);
-        int asciiB = (int) characterB;
-        if(asciiA >asciiB)
+        if(A.compareTo(B)>0)
         {
-            System.out.println("Yes");
+           System.out.println("Yes");
         }
         else
         {
             System.out.println("No");
         }
         char characterAu=Character.toUpperCase(characterA);
-        String characterAupper = characterAu+A.substring(1,5);
+        String characterAupper = characterAu+A.substring(1,A.length());
         char characterBu=Character.toUpperCase(characterB);
-        String characterBupper = characterBu+B.substring(1,4); 
+        String characterBupper = characterBu+B.substring(1,B.length()); 
         System.out.println(characterAupper+" "+characterBupper);
     }
 }
