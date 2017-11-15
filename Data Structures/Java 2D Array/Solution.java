0977f4a001378e4b4b0dd8fea4a1682627185ad7
Karan Shah <kshah2@lakeheadu.ca>
2017-11-15 14:34:02 -0400

    Update Solution.java

--- Data Structures/Java 2D Array/Solution.java
+++ Data Structures/Java 2D Array/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
             }
         }
         int result=0;
-        int max =0;
+        int max =-100;
         for(int i = 0; i<4; i++){
             for(int j =0; j<4; j++){
                 result = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
